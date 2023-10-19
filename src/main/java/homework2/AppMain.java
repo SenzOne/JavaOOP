5b139bd99f96d6341b64a72e29dd736a262e5e42
Senz One <sem63sam@yandex.ru>
2023-10-20 00:35:57 +0400

    [upd] homework 2

--- src/main/java/homework2/AppMain.java
+++ src/main/java/homework2/AppMain.java
@@ -4,7 +4,9 @@ public class AppMain {
     public static void main(String[] args) {
         Market market = new Market();
         Human human1 = new Human("Петя");
+        Human human2 = new Human("Вася");
         market.acceptToMarket(human1);
+        market.acceptToMarket(human2);
         market.update();
     }
 }
